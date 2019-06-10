560de6bc031b9654b231c2b96a1b8a188ea69961
Jason Do <Jason.Do1@hotmail.com>
2019-06-10 22:09:49 +1000

    minor code refactoring

--- word_ladder.cpp
+++ word_ladder.cpp
@@ -1,11 +1,18 @@
 #include <string>
 #include <set>
 #include <vector>
-#include <queue> 
+#include <queue>
+#include <iostream>
 
 #include "assignments/wl/word_ladder.h"
 
-std::string bfs(std::string src, std::string dest, std::set<std::string>& lex) {
+void PrintWordLadder(std::vector<std::string>& wl);
+void AddWordLadder(std::string nextWord, std::vector<std::string>& wl,
+                   std::queue<std::vector<std::string>>& wls,
+                   std::set<std::string>& visited);
+
+
+std::string Bfs(std::string src, std::string dest, std::set<std::string>& lex) {
   std::queue<std::vector<std::string>> wls;
   std::set<std::string> visited;
 
@@ -21,31 +28,56 @@ std::string bfs(std::string src, std::string dest, std::set<std::string>& lex) {
     // generate valid words
     int len = parentWord.length();
     for (int index = 0; index < len; index++) {
+
       for (int letter = 97; letter < 123; letter++)  {
         std::string newWord = parentWord;
         newWord = newWord.replace(index, 1, std::string(1, letter));
 
+        // Check new word is valid and not visited in a shorter path
+        if (lex.find(newWord) == lex.end() ||
+            visited.find(newWord) != visited.end()) continue;
+
         // goal state
         if (newWord.compare(dest) == 0) {
           wl.push_back(dest);
-          for (const auto&  word : wl) {
-            std::cout << word << " ";
-          }
-          std::cout << "\n";
-          return "finito";
+          PrintWordLadder(wl);
+          return dest;
         }
 
-        // not valid word or visited
-        if (lex.find(newWord) == lex.end() || visited.find(newWord) != visited.end()) continue;
-
-        // add new word ladder to queue
-        visited.insert(newWord);
-        std::vector<std::string> newWl = wl;
-        newWl.push_back(newWord);
-        wls.push(newWl);
+        AddWordLadder(newWord, wl, wls, visited);
       }
+
     }
   }
-  
-  return "No path exists";
+
+  return src;
 }
+
+/**
+ * Shows the word ladder
+ * @param wl
+ */
+void PrintWordLadder(std::vector<std::string>& wl) {
+  for (const auto& word : wl) {
+    std::cout << word << " ";
+  }
+  std::cout << "\n";
+}
+
+/**
+ * Append next word in the word ladder to the queue
+ * @param nextWord
+ * @param wl
+ * @param wls
+ * @param visited
+ */
+void AddWordLadder(std::string nextWord, std::vector<std::string>& wl,
+                   std::queue<std::vector<std::string>>& wls,
+                   std::set<std::string>& visited) {
+  visited.insert(nextWord);
+  std::vector<std::string> newWl = wl;
+  newWl.push_back(nextWord);
+  wls.push(newWl);
+
+}
+
