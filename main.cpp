cc7dc8bf6fa493679635f5a198409a904df54515
Jason Do <Jason.Do1@hotmail.com>
2019-06-10 20:02:56 +1000

    initial files

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+#include "assignments/wl/lexicon.h"
+#include "assignments/wl/word_ladder.h"
+
+int main() {
+  auto lexicon = GetLexicon("assignments/wl/words.txt");
+
+  return 0;
+}
---
+++ word_ladder.cpp
@@ -0,0 +1,6 @@
+#include <string>
+#include <set>
+#include <vector>
+#include <queue> 
+
+#include "assignments/wl/word_ladder.h"
