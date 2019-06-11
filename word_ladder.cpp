2d532f50313a4aaa03f66873105391711d9ab815
Jason Do <Jason.Do1@hotmail.com>
2019-06-11 16:45:22 +1000

    refactored code

--- word_ladder.cpp
+++ word_ladder.cpp
@@ -1,17 +1,20 @@
+#include <iostream>
+#include <algorithm>
 #include <string>
 #include <set>
 #include <vector>
 #include <queue>
-#include <iostream>
-#include <algorithm>
-#include <utility>
 #include <unordered_map>
 
 #include "assignments/wl/word_ladder.h"
 
-//TODO  share  shire  sharn     shire appears before sharn  (but need alphabet)  b/c  i changed before n
-// use .sort(start, end, comparator func)  at end
-
+/**
+ * Performs breadth first search to obtain all word ladders from src to dest
+ * @param src   start word of the word ladder
+ * @param dest  final word of the word ladder
+ * @param lex   lexicon
+ * @return      vector of word ladders
+ */
 std::vector<std::vector<std::string>> Bfs(std::string src, std::string dest, std::set<std::string>& lex) {
   std::queue<std::vector<std::string>> wls;
   std::vector<std::vector<std::string>> result;
@@ -22,42 +25,43 @@ std::vector<std::vector<std::string>> Bfs(std::string src, std::string dest, std
   visited.insert_or_assign(src, 1);
 
   while (!wls.empty()) {
-    std::vector<std::string> wl = wls.front();
+    const std::vector<std::string> wl = wls.front();
     wls.pop();
 
     /* End algorithm when curr ladder is longer than the shortest found ladder */
     curr_ladder_len = wl.size();
     if (curr_ladder_len >= min_ladder_len && min_ladder_len) break;
-    
-    std::string parentWord = wl.back();
 
-    // generate valid words
-    int len = parentWord.length();
+    const std::string parent_word = wl.back();
+    std::string newWord = wl.back();
+    std::vector<std::string> new_wl = wl;
+    const int len = parent_word.length();
+
+    /* Generate all possible valid words to extend the current word ladder */
     for (int index = 0; index < len; index++) {
 
-      // new function
       for (int letter = 97; letter < 123; letter++)  {
-        std::string newWord = parentWord;
+        /* Generate new word from parent word and append to word ladder */
+        newWord = parent_word;
         newWord = newWord.replace(index, 1, std::string(1, letter));
+        new_wl = wl;
+        new_wl.push_back(newWord);
 
-        auto search = visited.find(newWord);
-
-        // Check new word is valid and not visited in a shorter path
-        if (lex.find(newWord) == lex.end() ||
-            (search != visited.end() && search->second < curr_ladder_len)) continue;
-
-        std::vector<std::string> newWl = wl;
-        newWl.push_back(newWord);
-
-        // goal state
+        /* Destination word generated */
         if (newWord.compare(dest) == 0) {
-          result.push_back(newWl);
-          min_ladder_len = newWl.size();
+          result.push_back(new_wl);
+          min_ladder_len = new_wl.size();
           break;
+
+        /* Check new word is valid and not visited in a shorter path */
+        } else {
+          auto itr = visited.find(newWord);
+          if (lex.find(newWord) == lex.end() ||
+              (itr != visited.end() && itr->second < curr_ladder_len)) continue;
         }
 
         visited.insert_or_assign(newWord, wl.size());
-        wls.push(newWl);
+        wls.push(new_wl);
       }
 
     }
@@ -68,7 +72,7 @@ std::vector<std::vector<std::string>> Bfs(std::string src, std::string dest, std
 
 /**
  * Sort word ladders alphabetically
- * @param wls
+ * @param wls   vector of word ladders
  */
 void SortWordLadders(std::vector<std::vector<std::string>>& wls) {
   std::sort(wls.begin(), wls.end());
@@ -76,9 +80,14 @@ void SortWordLadders(std::vector<std::vector<std::string>>& wls) {
 
 /**
  * Shows all word ladders
- * @param wls
+ * @param wls   vector of word ladders
  */
 void PrintWordLadders(std::vector<std::vector<std::string>>& wls) {
+  if (wls.empty()) {
+    std::cout << "No ladder found.\n";
+    return;
+  }
+
   for (const auto& wl : wls) {
     PrintWordLadder(wl);
   }
@@ -86,7 +95,7 @@ void PrintWordLadders(std::vector<std::vector<std::string>>& wls) {
 
 /**
  * Shows the word ladder
- * @param wl
+ * @param wl    word ladder
  */
 void PrintWordLadder(const std::vector<std::string>& wl) {
   for (const auto& word : wl) {
